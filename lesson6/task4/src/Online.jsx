e792f4bcab2acbce688fed9112859a9560e2d628
iracholii <ira.cholii.m@gmail.com>
2022-06-01 15:14:34 +0300

    adding files

---
+++ lesson6/task4/src/Online.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Online = () => {
+  return (
+    <>
+      <span className="status__text">Online</span>
+      <button className="status__btn">All good!</button>
+    </>
+  );
+};
+
+export default Online;
---
+++ lesson6/task4/src/Status.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+const Status = (props) => {
+  return (
+    <div className="status">{props.isOnline ? <Online /> : <Offline />}</div>
+  );
+};
+
+export default Status;
