eea9a7268573e05158688f039a483f9c0968c3e0
iracholii <ira.cholii.m@gmail.com>
2022-06-07 20:25:55 +0300

    adding files

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Profile from './Profile';
+import ShoppingCart from './ShoppingCart';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class App extends React.Component {
+  state = {
+    userData: {
+      firstName: 'Jane',
+      lastName: 'Doe',
+    },
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userData } = this.state;
+
+    return (
+      <div className="page">
+        <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+        <main className="content">
+          <ShoppingCart userName={userData.firstName} />
+          <Profile userData={userData} handleChange={this.handleChange} />
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
