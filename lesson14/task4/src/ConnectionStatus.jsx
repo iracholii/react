1392f289cd1d12fe93394c915e2634d5d31beacb
iracholii <ira.cholii.m@gmail.com>
2022-06-16 15:24:14 +0300

    adding files

--- lesson14/task4/src/ConnectionStatus.jsx
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -1,20 +1,48 @@
 import React, { useState, useEffect } from 'react';
 
+// 1
+// const ConnectionStatus = () => {
+//   const [status, setStatus] = useState('online');
+
+//   const statusChangeHandler = () => {
+//     const changedStatus = status === 'online' ? 'offline' : 'online';
+//     setStatus(changedStatus);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('online', statusChangeHandler);
+//     window.addEventListener('offline', statusChangeHandler);
+
+//     return () => {
+//       window.removeEventListener('online', statusChangeHandler);
+//       window.removeEventListener('offline', statusChangeHandler);
+//     };
+//   });
+
+//   const classNames = status === 'online' ? 'status' : 'status status_offline';
+
+//   return <div className={classNames}>{status}</div>;
+// };
+
+// 2
 const ConnectionStatus = () => {
   const [status, setStatus] = useState('online');
 
-  const statusChangeHandler = () => {
-    const changedStatus = status === 'online' ? 'offline' : 'online';
-    setStatus(changedStatus);
+  const onlineHandler = () => {
+    setStatus('online');
+  };
+
+  const offlineHandler = () => {
+    setStatus('offline');
   };
 
   useEffect(() => {
-    window.addEventListener('online', statusChangeHandler);
-    window.addEventListener('offline', statusChangeHandler);
+    window.addEventListener('online', onlineHandler);
+    window.addEventListener('offline', offlineHandler);
 
     return () => {
-      window.removeEventListener('online', statusChangeHandler);
-      window.removeEventListener('offline', statusChangeHandler);
+      window.removeEventListener('online', onlineHandler);
+      window.removeEventListener('offline', offlineHandler);
     };
   });
 
