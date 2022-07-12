2d39ef60d17838b9a6b8a4dbf74f64ece8a28b01
iracholii <ira.cholii.m@gmail.com>
2022-07-12 10:49:16 +0300

    updating

--- lesson11/task4/src/withDataLoader.jsx
+++ lesson11/task4/src/withDataLoader.jsx
@@ -1,39 +1,78 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from './Spinner';
 
-const withDataLoader = url => {
-  const func = Component => {
-    return class HOC extends React.Component {
-      state = {
-        data: null,
-        spinner: true,
-        component: false,
-      };
+// Version 1
 
-      componentDidMount() {
-        fetch(url)
-          .then(response => response.json())
-          .then(data =>
-            this.setState({
-              data,
-              spinner: false,
-              component: true,
-            }),
-          );
-      }
+// const withDataLoader = url => {
+//   const HOC = Component => {
+//     return class EnhancedComponent extends React.Component {
+//       state = {
+//         data: null,
+//         spinner: true,
+//         component: false,
+//       };
 
-      render() {
-        return (
-          <>
-            {this.state.spinner && <Spinner />}
-            {this.state.component && <Component data={this.state.data} />}
-          </>
+//       componentDidMount() {
+//         fetch(url)
+//           .then(response => response.json())
+//           .then(data =>
+//             this.setState({
+//               data,
+//               spinner: false,
+//               component: true,
+//             }),
+//           );
+//       }
+
+//       render() {
+//         const { data, spinner, component } = this.state;
+
+//         return (
+//           <>
+//             {spinner && <Spinner />}
+//             {component && <Component data={data} />}
+//           </>
+//         );
+//       }
+//     };
+//   };
+
+//   return HOC;
+// };
+
+// Version 2
+
+const withDataLoader = url => Component => {
+  const EnhancedComponent = () => {
+    const [state, setState] = useState({
+      data: null,
+      spinner: true,
+      component: false,
+    });
+
+    useEffect(() => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data =>
+          setState({
+            data,
+            spinner: false,
+            component: true,
+          }),
         );
-      }
-    };
+    }, []);
+
+    const { data, spinner, component } = state;
+
+    return (
+      <>
+        {spinner && <Spinner />}
+        {component && <Component data={data} />}
+      </>
+    );
   };
-  return func;
+
+  return EnhancedComponent;
 };
 
 export default withDataLoader;
